eb7c4864997631ad5ade0b65c48d860612b5bf5e
Sapphire Godard <cng8060@g.rit.edu>
2024-09-16 19:31:43 -0400

    - Added the String token and a processing function inside of it that will handle parsing Strings

--- src/group22/String.java
+++ src/group22/String.java
@@ -1,4 +1,60 @@
 package group22;
 
-public class String {
+import provided.Token;
+import provided.TokenType;
+
+import java.io.FileReader;
+import java.io.IOException;
+
+public class String extends Token {
+
+    private final java.lang.String value;
+
+    /**
+     * Creates an instance of a token
+     *
+     * @param token    the token string
+     * @param filename the name of the file the token came from
+     * @param lineNum  the number of the line in the file that the token appears on
+     * @param type     the type of this token
+     */
+    public String(java.lang.String token,
+                  java.lang.String filename,
+                  int lineNum,
+                  TokenType type,
+                  java.lang.String value
+    ) {
+        super(token, filename, lineNum, type);
+        this.value = value;
+    }
+
+    public java.lang.String getValue() {
+        return value;
+    }
+
+    public static boolean isValidStringCharacter(char input) {
+        return (input == ' ') ||
+                (input >= 'a' && input <= 'z') ||
+                (input >= 'A' && input <= 'Z') ||
+                (input >= '0' && input <= '9');
+    }
+
+    public static String processString(FileReader inputStream) throws IOException {
+        java.lang.String currString = "";
+        int currChar;
+
+        while((currChar = inputStream.read()) != -1) {
+            if (!isValidStringCharacter((char) currChar)) {
+                currString += (char) currChar; // This won't be used yet
+            } else {
+                if (currChar == '"') {
+                    return new String(null, null, 0, null, currString);
+                } else {
+                    break;
+                }
+            }
+        }
+
+        return SyntaxException();
+    }
 }
