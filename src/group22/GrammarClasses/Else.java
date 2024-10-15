fd3258d1573741b2b07e4f386d24401847dcab84
Andrei Makarov <aom3091@g.rit.edu>
2024-10-15 14:50:28 -0400

    Mostly implemented Else

--- src/group22/GrammarClasses/Else.java
+++ src/group22/GrammarClasses/Else.java
@@ -1,4 +1,47 @@
 package group22.GrammarClasses;
+import group22.SyntaxException;
+import provided.*;
 
-public class Else {
+import java.util.ArrayList;
+
+public class Else implements JottTree{
+    private final Body bodyNode;
+
+    public Else(Body bodyNode) {
+        this.bodyNode = bodyNode;
+    }
+
+    static Else parseElse(ArrayList<Token> tokens) throws SyntaxException {
+        if (tokens.isEmpty()) {
+            throw new SyntaxException("Unexpected eof");
+        }
+        Token currToken = tokens.getFirst();
+        if(!(currToken.getTokenType() == TokenType.ID_KEYWORD && currToken.getToken().equals("Else"))) {
+            return new Else(null);
+        }
+        if(!(currToken.getTokenType() == TokenType.L_BRACE && currToken.getToken().equals("{"))) {
+            throw new SyntaxException("Expected left brace", currToken.getFilename(), currToken.getLineNum());
+        }
+        Body bodyNode = Body.parseBody(tokens);
+        if(!(currToken.getTokenType() == TokenType.R_BRACE && currToken.getToken().equals("}"))) {
+            throw new SyntaxException("Expected right brace", currToken.getFilename(), currToken.getLineNum());
+        }
+        return new Else(bodyNode);
+    }
+
+    public String convertToJott() {
+        if (bodyNode == null) {
+            return "";
+        }
+        return ("Else{" + bodyNode.convertToJott() + "}");
+    }
+
+    public boolean validateTree() {
+        //TODO
+        return false;
+    }
+
+    public void execute() {
+        //TODO
+    }
 }
