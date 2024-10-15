8ac3a18fae6d087492e049de59d0c71fc73f1519
Andrei Makarov <aom3091@g.rit.edu>
2024-10-15 13:28:59 -0400

    Mostly implemented parseFuncCall

--- src/group22/GrammarClasses/FuncCall.java
+++ src/group22/GrammarClasses/FuncCall.java
@@ -1,4 +1,54 @@
 package group22.GrammarClasses;
+import group22.SyntaxException;
+import provided.*;
 
-public class FuncCall {
+import java.util.ArrayList;
+
+public class FuncCall implements JottTree{
+    private final Id idNode;
+    private final Params paramsNode;
+
+    public FuncCall(Id idNode, Params paramsNode) {
+        this.idNode = idNode;
+        this.paramsNode = paramsNode;
+    }
+
+    static FuncCall parseFuncCall(ArrayList<Token> tokens) throws SyntaxException {
+        if (tokens.isEmpty()) {
+            throw new SyntaxException("Unexpected eof");
+        }
+        Token currToken = tokens.getFirst();
+        if (currToken.getTokenType() != TokenType.COLON) {
+            throw new SyntaxException("Should be a colon", currToken.getFilename(), currToken.getLineNum());
+        }
+        tokens.removeFirst();
+        if (currToken.getTokenType() != TokenType.COLON) {
+            throw new SyntaxException("Should be a colon", currToken.getFilename(), currToken.getLineNum());
+        }
+        tokens.removeFirst();
+        Id tempIdNode = Id.parseIDNode(tokens);
+        if (currToken.getTokenType() != TokenType.L_BRACKET) {
+            throw new SyntaxException("Should be a left bracket", currToken.getFilename(), currToken.getLineNum());
+        }
+        tokens.removeFirst();
+        Params tempParamsNode = Params.parseParamsNode(tokens);
+        if (currToken.getTokenType() != TokenType.R_BRACKET) {
+            throw new SyntaxException("Should be a right bracket", currToken.getFilename(), currToken.getLineNum());
+        }
+        tokens.removeFirst();
+        return new FuncCall(tempIdNode, tempParamsNode);
+    }
+
+    public String convertToJott() {
+        return ("::" + this.idNode.convertToJott() + "[" + this.paramsNode.convertToJott() + "]");
+    }
+
+    public boolean validateTree() {
+        //TODO
+        return false;
+    }
+
+    public void execute() {
+        //TODO
+    }
 }
