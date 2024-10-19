efe1f978ea1ea543100ca170abe4f720951a4b36
Andrei Makarov <aom3091@g.rit.edu>
2024-10-18 20:54:23 -0400

    EOF and other fixes to my nodes

--- src/group22/GrammarClasses/Else.java
+++ src/group22/GrammarClasses/Else.java
@@ -13,25 +13,29 @@ public class Else implements JottTree{
 
     static Else parse(ArrayList<Token> tokens) throws SyntaxException {
         if (tokens.isEmpty()) {
-            throw new SyntaxException("Unexpected EOF", "", -1);
+            throw new SyntaxException("Unexpected EOF", JottParser.getFileName(), JottParser.getLineNumber());
         }
-        Token currToken = tokens.get(0);
-        if(!(currToken.getTokenType() == TokenType.ID_KEYWORD && currToken.getToken().equals("Else"))) {
-            return new Else(null);
+        try {
+            Token currToken = tokens.get(0);
+            if (!(currToken.getTokenType() == TokenType.ID_KEYWORD && currToken.getToken().equals("Else"))) {
+                return new Else(null);
+            }
+            tokens.remove(0);
+            currToken = tokens.get(0);
+            if (!(currToken.getTokenType() == TokenType.L_BRACE && currToken.getToken().equals("{"))) {
+                throw new SyntaxException("Expected left brace", currToken.getFilename(), currToken.getLineNum());
+            }
+            tokens.remove(0);
+            Body bodyNode = Body.parse(tokens);
+            currToken = tokens.get(0);
+            if (!(currToken.getTokenType() == TokenType.R_BRACE && currToken.getToken().equals("}"))) {
+                throw new SyntaxException("Expected right brace", currToken.getFilename(), currToken.getLineNum());
+            }
+            tokens.remove(0);
+            return new Else(bodyNode);
+        } catch (IndexOutOfBoundsException e) {
+            throw new SyntaxException("Unexpected EOF", JottParser.getFileName(), JottParser.getLineNumber());
         }
-        tokens.remove(0);
-        currToken = tokens.get(0);
-        if(!(currToken.getTokenType() == TokenType.L_BRACE && currToken.getToken().equals("{"))) {
-            throw new SyntaxException("Expected left brace", currToken.getFilename(), currToken.getLineNum());
-        }
-        tokens.remove(0);
-        Body bodyNode = Body.parse(tokens);
-        currToken = tokens.get(0);
-        if(!(currToken.getTokenType() == TokenType.R_BRACE && currToken.getToken().equals("}"))) {
-            throw new SyntaxException("Expected right brace", currToken.getFilename(), currToken.getLineNum());
-        }
-        tokens.remove(0);
-        return new Else(bodyNode);
     }
 
     public String convertToJott() {
--- src/group22/GrammarClasses/ElseIf.java
+++ src/group22/GrammarClasses/ElseIf.java
@@ -15,36 +15,40 @@ public class ElseIf implements JottTree{
 
     static ElseIf parse(ArrayList<Token> tokens) throws SyntaxException {
         if (tokens.isEmpty()) {
-            throw new SyntaxException("Unexpected EOF", "", -1);
+            throw new SyntaxException("Unexpected EOF", JottParser.getFileName(), JottParser.getLineNumber());
         }
-        Token currToken = tokens.get(0);
-        if(!(currToken.getTokenType() == TokenType.ID_KEYWORD && currToken.getToken().equals("Elseif"))) {
-            throw new SyntaxException("Expected Elseif keyword", currToken.getFilename(), currToken.getLineNum());
+        try {
+            Token currToken = tokens.get(0);
+            if (!(currToken.getTokenType() == TokenType.ID_KEYWORD && currToken.getToken().equals("Elseif"))) {
+                throw new SyntaxException("Expected Elseif keyword", currToken.getFilename(), currToken.getLineNum());
+            }
+            tokens.remove(0);
+            currToken = tokens.get(0);
+            if (!(currToken.getTokenType() == TokenType.L_BRACKET && currToken.getToken().equals("["))) {
+                throw new SyntaxException("Expected left bracket", currToken.getFilename(), currToken.getLineNum());
+            }
+            tokens.remove(0);
+            Expr exprNode = Expr.parse(tokens);
+            currToken = tokens.get(0);
+            if (!(currToken.getTokenType() == TokenType.R_BRACKET && currToken.getToken().equals("]"))) {
+                throw new SyntaxException("Expected right bracket", currToken.getFilename(), currToken.getLineNum());
+            }
+            tokens.remove(0);
+            currToken = tokens.get(0);
+            if (!(currToken.getTokenType() == TokenType.L_BRACE && currToken.getToken().equals("{"))) {
+                throw new SyntaxException("Expected left brace", currToken.getFilename(), currToken.getLineNum());
+            }
+            tokens.remove(0);
+            Body bodyNode = Body.parse(tokens);
+            currToken = tokens.get(0);
+            if (!(currToken.getTokenType() == TokenType.R_BRACE && currToken.getToken().equals("}"))) {
+                throw new SyntaxException("Expected right brace", currToken.getFilename(), currToken.getLineNum());
+            }
+            tokens.remove(0);
+            return new ElseIf(exprNode, bodyNode);
+        } catch (IndexOutOfBoundsException e) {
+            throw new SyntaxException("Unexpected EOF", JottParser.getFileName(), JottParser.getLineNumber());
         }
-        tokens.remove(0);
-        currToken = tokens.get(0);
-        if(!(currToken.getTokenType() == TokenType.L_BRACKET && currToken.getToken().equals("["))) {
-            throw new SyntaxException("Expected left bracket", currToken.getFilename(), currToken.getLineNum());
-        }
-        tokens.remove(0);
-        Expr exprNode = Expr.parse(tokens);
-        currToken = tokens.get(0);
-        if(!(currToken.getTokenType() == TokenType.R_BRACKET && currToken.getToken().equals("]"))) {
-            throw new SyntaxException("Expected right bracket", currToken.getFilename(), currToken.getLineNum());
-        }
-        tokens.remove(0);
-        currToken = tokens.get(0);
-        if(!(currToken.getTokenType() == TokenType.L_BRACE && currToken.getToken().equals("{"))) {
-            throw new SyntaxException("Expected left brace", currToken.getFilename(), currToken.getLineNum());
-        }
-        tokens.remove(0);
-        Body bodyNode = Body.parse(tokens);
-        currToken = tokens.get(0);
-        if(!(currToken.getTokenType() == TokenType.R_BRACE && currToken.getToken().equals("}"))) {
-            throw new SyntaxException("Expected right brace", currToken.getFilename(), currToken.getLineNum());
-        }
-        tokens.remove(0);
-        return new ElseIf(exprNode, bodyNode);
     }
 
     public String convertToJott() {
