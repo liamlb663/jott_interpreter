da134700e9fa235f5672c5bcec2b77272640ca81
Sapphire Godard <cng8060@g.rit.edu>
2024-12-06 15:10:03 -0500

    - Got rid of unneeded getValue() function in Bool

--- src/group22/GrammarClasses/Bool.java
+++ src/group22/GrammarClasses/Bool.java
@@ -39,10 +39,6 @@ public class Bool implements JottTree {
         return true;
     }
 
-    public boolean getValue() {
-        return bool.getToken().equals("True");
-    }
-
     public void execute() {
     }
 }
